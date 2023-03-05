e3e76458470a0c750df89fd531ab545400c0e098
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2023-03-04 17:23:03 -0700

    Configure tailwindcss to use dark theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
