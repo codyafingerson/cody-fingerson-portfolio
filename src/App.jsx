923137992a55089bbb4322813b218259a69416f8
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2023-03-04 17:24:41 -0700

    Configure router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Global components
+import NavBar from './components/NavBar';
+
+// Page importds
+import IndexPage from "./pages/IndexPage";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+      </Routes>
+    </Router>
   )
 }
 
