e5e60cda9c3a9de1d1d630e238d8172645f3a365
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2023-03-04 19:36:32 -0700

    Configure navbar to be responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,35 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function NavBar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900 relative z-10">
             <div className="container flex flex-wrap items-center justify-between mx-auto">
                 <Link to="/" className="flex items-center">
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Cody Fingerson</span>
                 </Link>
-                
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                <button
+                    className="inline-flex p-2 rounded-md md:hidden text-gray-500 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
+                    onClick={toggleMenu}
+                >
+
+                    <span className="sr-only">Open main menu</span>
+                    {/* Icon when menu is closed. */}
+                    <svg className={`block h-6 w-6 ${menuOpen ? 'hidden' : ''}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                    {/* Icon when menu is open. */}
+                    <svg className={`block h-6 w-6 ${menuOpen ? '' : 'hidden'}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+                <div className={`w-full md:block md:w-auto ${menuOpen ? '' : 'hidden'}`} id="navbar-default">
                     <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <Link to="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</Link>
