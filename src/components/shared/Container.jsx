30d4dc8748611de50c8fbc1be309a7097e187c19
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2023-03-04 19:51:02 -0700

    Add responsive tailwindclasses to Container.jsx

--- src/components/shared/Container.jsx
+++ src/components/shared/Container.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 function Container({ children }) {
     return (
         <div className="mt-auto">
-            <div className="absolute top-20 left-1/2 transform -translate-x-1/2 z-0 w-1/2">
+            <div className="absolute top-20 left-1/2 transform -translate-x-1/2 z-0 w-full md:w-1/2">
                 {children}
             </div>
             
