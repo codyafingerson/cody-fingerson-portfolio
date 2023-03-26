c4a5d994a930f37593e10e6eab071e991c278707
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2023-03-25 18:21:23 -0600

    Add resume to page

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,13 +1,18 @@
-import Container from "../components/shared/Container"
+import Container from "../components/shared/Container";
+import pdf from "../assets/fingerson_cody_resume.pdf";
 
 function ResumePage() {
   return (
     <Container>
-        <h1 className="text-4xl font-bold border-b-2 border-gray-700">
-            Resume
-        </h1>
+      <h1 className="text-4xl font-bold border-b-2 border-gray-700">Resume</h1>
+      <div className="flex flex-col items-center justify-center">
+        <iframe src={pdf} frameborder="0" 
+          className="w-full h-screen"
+        ></iframe>
+      </div>
+      
     </Container>
-  )
+  );
 }
 
-export default ResumePage
+export default ResumePage;
