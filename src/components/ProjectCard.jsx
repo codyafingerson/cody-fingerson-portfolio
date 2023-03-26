46dd9fc18ae4f28eba28097bb11df9755e674a56
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2023-03-25 18:20:41 -0600

    Allow project url to not display if no projectUrl is defined.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 function ProjectCard({ projectTitle, projectDescription, projectUrl }) {
     return (
-        <div class="mt-4 rounded-lg overflow-hidden shadow-lg">
-            <div class="p-4">
-                <h2 class="text-lg font-bold mb-2">{projectTitle}</h2>
-                <p class="text-base">{projectDescription}</p>
-                <Link to={projectUrl} target="_blank" rel="noopener noreferrer" class="mt-4 inline-block bg-blue-500 text-white rounded-lg px-4 py-2 hover:bg-blue-600">View Project</Link>
+        <div className="mt-4 rounded-lg overflow-hidden shadow-lg">
+            <div className="p-4">
+                <h2 className="text-lg font-bold mb-2">{projectTitle}</h2>
+                <p className="text-base">{projectDescription}</p>
+                {projectUrl ? <Link to={projectUrl} target="_blank" rel="noopener noreferrer" className="mt-4 inline-block bg-blue-500 text-white rounded-lg px-4 py-2 hover:bg-blue-600">View Project</Link> : ""}
             </div>
         </div>
     );
@@ -16,7 +16,6 @@ function ProjectCard({ projectTitle, projectDescription, projectUrl }) {
 ProjectCard.propTypes = {
     projectTitle: PropTypes.string.isRequired,
     projectDescription: PropTypes.string.isRequired,
-    projectUrl: PropTypes.string.isRequired,
 };
 
 export default ProjectCard;
